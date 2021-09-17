b4316467c73017008177ee77b483c877e7be6d9c
Eryk Słociński <eryk.slocinski@gmail.com>
2021-09-17 11:29:06 +0200

    feat: project configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,31 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "sobre-shop-cart",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'sobre-shop-cart',
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        components: path.join(__dirname, 'src/components'),
+        layouts: path.join(__dirname, 'src/layouts'),
+        images: path.join(__dirname, 'src/images'),
+        scss: path.join(__dirname, 'src/scss'),
+        types: path.join(__dirname, 'src/types'),
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+        utils: path.join(__dirname, 'src/utils'),
       },
     },
   ],
